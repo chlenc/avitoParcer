3261dd6515d3dd0cbf408bf0ad05a0d19d49cb90
chlenc <alexnagorny.an@gmail.com>
2021-01-09 16:02:56 +0300

    done

--- main.js
+++ main.js
@@ -1,66 +1,69 @@
 const TelegramBot = require('node-telegram-bot-api');
-const token = '760978574:AAGPNOfbFeo7X07I2MFJ8s02kiurNmisQUM';
-const bot = new TelegramBot(token, {polling: true});
-const groupId = '-351595481';
+const bot = new TelegramBot("1437222649:AAFQ89PJuXYHjK8IVQ4n2otWEZMS_DxYC7M", {polling: true});
+// const bot = new TelegramBot(process.env.TOKEN, {webHook: {port: +process.env.PORT}})
+// bot.setWebHook(`${process.env.URL}/bot${process.env.TOKEN}`).catch(e => console.error(e));
+const groupId = '-490363061';
 const parcer = require('node-html-parser');
 const axois = require('axios');
 
 const firebase = require("firebase");
 firebase.initializeApp({
     serviceAccount: "./serviceAccount.json",
-    databaseURL: 'https://rent-manager-bot.firebaseio.com/'
+    databaseURL: 'https://astrobot-26975.firebaseio.com/'
 });
 
-const targetAddress = 'Берсеневская набережная, 4, с3, Москва, Россия';
-const maxPrice = 40000;
 
-const URL = `https://www.avito.ru/moskva/kvartiry/sdam/na_dlitelnyy_srok?cd=1&pmax=${maxPrice}&pmin=0&user=1&s=104`;
+const URL = `https://www.avito.ru/rossiya/odezhda_obuv_aksessuary/kupit-aksessuary-ASgBAgICAUTeAtoL?q=%D1%81%D1%83%D0%BC%D0%BA%D0%B0+Chanel+%D0%BE%D1%80%D0%B8%D0%B3%D0%B8%D0%BD%D0%B0%D0%BB&s=104`;
 
-setTimeout(syncOffers, 2, 60 * 60 * 1000);
+setInterval(syncOffers, 30 * 1000);
 
 bot.onText(/\/echo/, msg => {
     bot.sendMessage(msg.chat.id, msg.chat.id);
 });
 
-function syncOffers() {
-    firebase.database().ref("offersIdArray").once("value", function (snapshot) {
+async function syncOffers() {
+    await firebase.database().ref("offersIdArray").once("value", async function (snapshot) {
         const offersIdArray = snapshot.val() || [];
-        axois.get(URL).then(res => {
-            const root = parcer.parse(res.data);
-            const data = (root).querySelectorAll('.item__line');
-            const result = data.map(home => {
-                const
-                    descriptionNode = home.querySelector('.item-description-title-link'),
-                    priceNode = home.querySelector('.price'),
-                    addressNode = home.querySelector('.item-address__string');
-                let link, title, price, address, route;
-
-                descriptionNode.rawAttrs.split('\n').forEach((val) => {
-                    let match;
-                    if ((match = val.match(/href="(.+)"/)) != null) link = `https://www.avito.ru${match[1]}`;
-                    if ((match = val.match(/title="(.+)"/)) != null) title = match[1]
-                });
-                const id = link.slice(-10)
-                if (offersIdArray.includes(id)) return null;
-
-                price = priceNode.childNodes[0].rawText.replace('\n', '');
-                address = addressNode.childNodes[0].rawText.replace('\n', '');
-                route = (`https://www.google.ae/maps/dir/${address}/${targetAddress}`)
-                    .replace(/ /g, '%20').replace(/,/g, '%2C');
-
-                return {id, link, title, price, address, route}
-            }).filter(val => val != null).map(({id, link, title, price, address, route}) => {
-                bot.sendMessage(groupId, `${title}: ${price} Рублей в месяц\nАдрес: ${address}`, {
+        const res = await axois.get(URL)
+        const root = parcer.parse(res.data);
+        const data = root.querySelectorAll('.js-catalog-item-enum');
+        const parsedParams = data.map(home => {
+            try {
+                const id = home.id
+                const href = home.firstChild.childNodes[0].lastChild.rawAttrs.match(/href="(.+?)"/)[1]
+                const link = 'https://www.avito.ru' + href
+                const title = home.firstChild.childNodes[1].childNodes[0].firstChild.firstChild.text
+                const price = home.firstChild.childNodes[1].childNodes[1].firstChild.firstChild.lastChild.text
+                return {id, link, title, price}
+            } catch (e) {
+                console.error(e)
+                return null
+            }
+        }).filter(v =>
+            v !== null
+            && (v.title.split(' ').includes('chanel') || v.title.split(' ').includes('Chanel'))
+            && !offersIdArray.includes(v.id)
+        )
+        const result = []
+        for (let i in parsedParams) {
+            try {
+                const {id, link, title, price} = parsedParams[i]
+                await bot.sendMessage(groupId, `${title}: ${price}`, {
                     reply_markup: {
-                        inline_keyboard: [[{text: 'Ссылка на авито', url: link}], [{text: 'Маршрут', url: route}]]
+                        inline_keyboard: [[{text: 'Ссылка на авито', url: link}]]
                     }
                 });
-                return id
-            });
-
-            firebase.database().ref('/').update({offersIdArray: offersIdArray.concat(result)})
-        })
+                await new Promise(r => (setTimeout(r, 1000)))
+                result.push(id)
+            } catch (e) {
+                console.error(e)
+            }
+        }
+        await firebase.database().ref('/')
+            .update({offersIdArray: offersIdArray.concat(result)})
+        console.log(result)
     })
 }
 
 
+console.log('bot was started')
